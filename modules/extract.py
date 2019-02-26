897c8afbfbf931ccbe432e1cd4f9229ae679ca0c
Richie Zhang <richiezhang256@gmail.com>
2019-02-26 12:02:07 -0800

    Call the extractor function for a known extractor and raise an error othewise.

---
+++ modules/extract.py
@@ -0,0 +1,7 @@
+class ExtractionError(Exception):
+    '''The exception that is raised when there is an error when extracting.'''
+
+def extract_7z(path:str):
+    '''Extract a file using 7zip.
+
+    '''
--- startup.py
+++ startup.py
@@ -1,28 +1,35 @@
 # The main startup file
 
 import argparse
+from modules.extract import *
 
-def startup(path:str, extractor:str='7zip'):
+def startup(path:str, extractor:str='7z'):
     '''The program startup function.
 
     Arguments:
      - path: The path of the file to extract (only one path at a time)
      - extractor: The extraction software to use.
     '''
+    # Check the extraction software selection
+    if extractor == '7z':
+        extract_7z(path)
+    else:
+        raise ExtractionError(f'The extraction software \'{extractor}\' is not recognized.')
 
-    print(f'path:\t\t{path}')
-    print(f'extractor:\t{extractor}')
-    print()
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Recursively extract a file.')
     parser.add_argument('paths', type=str, nargs='+',
                         help='A list of file paths to extract.')
-    parser.add_argument('--extractor', type=str, default='7zip',
+    parser.add_argument('--extractor', type=str, default='7z',
                         help='The extraction software to use (tar, zip, etc...)')
 
     args = parser.parse_args()
-    print(args)
 
     for path in args.paths:
-        startup(path, args.extractor)
+        try:
+            startup(path, args.extractor)
+        except ExtractionError as e:
+            print(str(e))
+            break
